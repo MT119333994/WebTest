50a84695847f78ab7eec8d66e0448d362adab2fd
MaTeng <119333994@qq.com>
2014-09-10 02:32:16 +0800

    无用代码去除

--- dist/js/index/index.js
+++ dist/js/index/index.js
@@ -1,14 +1,3 @@
 /**
  * Created by MaTeng on 2014/9/10.
  */
-$(function () {
-    $('#header').load('/WebTest/dist/template/header-template.html #header');
-});
-
-(function (window) {
-    var global = window.global = {};
-
-    var activeNav = function (section) {
-
-    };
-})(window);
