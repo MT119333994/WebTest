4d48c12a53031b3e882690d7119eb06f04552c3a
redsunlei <316957922@qq.com>
2014-09-13 16:03:05 +0800

    [代码删除] 添加活动

--- dist/js/promotion/promotion.js
+++ dist/js/promotion/promotion.js
@@ -1,3 +1,9 @@
 /**
  * Created by MaTeng on 2014/9/10.
  */
+$(function () {
+    $(".nav-filter li").on("click",function(){
+        $(this).addClass("cur");
+        $(this).siblings().removeClass("cur");
+    })
+});
