943d7c0be65467dd60a63576050879f74227c357
MaTeng <119333994@qq.com>
2014-09-21 21:48:07 +0800

    将改版后的页面作为新版皮肤，右上角提供切换皮肤对比功能

--- dist/js/global/global.js
+++ dist/js/global/global.js
@@ -2,22 +2,68 @@
  * Created by MaTeng on 2014/9/9.
  */
 
-$(function () {
-    $.ajax({
-        url: "/WebTest/dist/template/global-template.html",
-        dataType: "html"
-    }).done(function (responseText) {
-        var $response = $("<div>").append($.parseHTML(responseText));
-        $('#header').html($response.find('#header-template').html());
+(function (window) {
+    // 全局对象
+    var global = window.global = {};
 
-        var category = window.location.pathname.split('/');
-        if (category[category.length - 1] == 'index.html') {
-            $('#index').addClass('active');
-            $('#category-panel').removeClass('hide');
+    // 新旧皮肤模板URL
+    var headerTemplateUrl = {
+        urlNew: "/WebTest/dist/template/global-template-20140921.html",
+        urlOld: "/WebTest/dist/template/global-template.html"
+    };
+
+    // 获取头部和页脚模板
+    var getHeader = function () {
+        var skinType = window.sessionStorage.getItem("index-skin-type");
+        var url;
+        if (skinType == null || skinType == "") {
+            url = headerTemplateUrl.urlNew;
+        } else if (skinType == "new") {
+            url = headerTemplateUrl.urlOld
         } else {
-            $('#' + category[category.length - 2]).addClass('active');
+            url = headerTemplateUrl.urlNew;
         }
 
-        $('#footer').html($response.find('#footer-template').html());
-    });
+        $.ajax({
+            url: url,
+            dataType: "html",
+            async: false
+        }).done(function (responseText) {
+            var $response = $("<div>").append($.parseHTML(responseText));
+            $('#header').html($response.find('#header-template').html());
+
+            var category = window.location.pathname.split('/');
+            if (category[category.length - 1] == 'index.html') {
+                $('#index').addClass('active');
+                $('#category-panel').removeClass('hide');
+            } else {
+                $('#' + category[category.length - 2]).addClass('active');
+            }
+
+            $('.change-skin').on('click', function () {
+                changeSkin();
+                window.location.reload();
+            });
+
+            $('#footer').html($response.find('#footer-template').html());
+        });
+    };
+
+    // 切换皮肤设置
+    var changeSkin = function () {
+        var skinType = window.sessionStorage.getItem("index-skin-type");
+        if (skinType == null || skinType == "") {
+            window.sessionStorage.setItem("index-skin-type", "new");
+        } else if (skinType == "new") {
+            window.sessionStorage.setItem("index-skin-type", "old");
+        } else {
+            window.sessionStorage.setItem("index-skin-type", "new");
+        }
+    };
+
+    global.getHeader = getHeader;
+})(window);
+
+$(function () {
+    global.getHeader();
 });
