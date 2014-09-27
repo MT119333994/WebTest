680e02780f6e57ff9f5339b6b7ebe5e23ca5d23c
redsunlei <316957922@qq.com>
2014-09-27 16:51:13 +0800

    修改商场活动

--- dist/js/promotion/promotion.js
+++ dist/js/promotion/promotion.js
@@ -2,8 +2,14 @@
  * Created by MaTeng on 2014/9/10.
  */
 $(function () {
+    // 老版本使用
     $(".nav-filter li").on("click",function(){
         $(this).addClass("cur");
         $(this).siblings().removeClass("cur");
-    })
+    });
+    // 新版本使用
+    $(".nav-pills li").on("click",function(){
+        $(this).addClass("cur");
+        $(this).siblings().removeClass("cur");
+    });
 });
