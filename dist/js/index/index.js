897d97128c22f632ed97b0439e198e55b9f28340
MaTeng <119333994@qq.com>
2014-09-29 01:31:49 +0800

    首页手机数码商品展示部分

--- dist/js/index/index.js
+++ dist/js/index/index.js
@@ -6,4 +6,30 @@ $(function () {
     $(".floor-title .floor-title-tab").on("mouseover", "a", function () {
         $(this).parent().addClass("active").siblings(".active").removeClass("active");
     });
+
+    $("#fc-category-tab").on("mouseenter", "li", function () {
+        $(this).addClass("selected").siblings(".selected").removeClass("selected");
+        var animateDisplace;
+        var selectedIndex = $(this).index();
+        switch (selectedIndex) {
+            case 0 :
+                animateDisplace = "0";
+                break;
+            case 1 :
+                animateDisplace = "-250px";
+                break;
+            case 2 :
+                animateDisplace = "-500px";
+                break;
+            case 3 :
+                animateDisplace = "-750px";
+                break;
+            case 4 :
+                animateDisplace = "-1000px";
+                break;
+            default :
+                animateDisplace = "0";
+        }
+        $(".fc-category-wrapper").css("top", animateDisplace);
+    });
 });
