1a9942e4c8152b98634d177fa7043b24e8314ebf
MT119333994 <119333994@qq.com>
2014-10-23 21:29:24 +0800

    价格速递首页调整

--- dist/js/products/products.js
+++ dist/js/products/products.js
@@ -11,4 +11,10 @@ $(function () {
     $(".promotion-nav .promotion-nav-item").on("click", function () {
         $(this).parent().toggleClass("selected").siblings(".selected").removeClass("selected");
     });
+
+    $(".promotion-list").on("mouseover", "a", function(){
+        $(this).addClass("item-link-hover");
+    }).on("mouseout", "a", function(){
+        $(this).removeClass("item-link-hover");
+    });
 });
